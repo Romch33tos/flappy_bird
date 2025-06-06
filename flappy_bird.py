dd542f3fa4fcbfecafecc04a004df7a1de6e7489
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-06 17:32:08 +0700

    Исправления для движения объектов

--- flappy_bird.py
+++ flappy_bird.py
@@ -6,23 +6,18 @@ class FlappyBird:
   def __init__(self, master):
     self.master = master
     self.master.title("Flappy Bird")
-
     self.pipe_image = Image.open("Python/Программы/Игры/FB/game_files/p1.png")
     self.pipe_image = self.pipe_image.resize((50, 400))
     self.pipe_image = ImageTk.PhotoImage(self.pipe_image)
-
     self.bird_image = Image.open("Python/Программы/Игры/FB/game_files/b.png")
     self.bird_image = self.bird_image.resize((50, 50))
     self.bird_image = ImageTk.PhotoImage(self.bird_image)
-
     self.width = 400
     self.height = 600
     self.gravity = 0.5
     self.jump_strength = 8.5
-
     self.canvas = tk.Canvas(master, width=self.width, height=self.height, bg="skyblue")
     self.canvas.pack()
-
     self.score = 0
     self.game_over = False
     self.bird = None
@@ -31,9 +26,7 @@ class FlappyBird:
     self.start_message = None
     self.game_over_message = None
     self.score_text = None
-
     self.master.bind("<ButtonPress-1>", self.on_click)
-
     self.show_start_message()
 
   def show_start_message(self):
@@ -83,27 +76,22 @@ class FlappyBird:
     if not self.game_over:
       self.bird_y_velocity += self.gravity
       self.canvas.move(self.bird, 0, self.bird_y_velocity)
-
       for top_pipe, bottom_pipe in self.pipes:
         self.canvas.move(top_pipe, -3, 0)
         self.canvas.move(bottom_pipe, -3, 0)
-
       if self.pipes and self.canvas.coords(self.pipes[0][0])[0] < 0:
         top_pipe, bottom_pipe = self.pipes.pop(0)
         self.canvas.delete(top_pipe)
         self.canvas.delete(bottom_pipe)
         self.score += 1
-
       if len(self.pipes) == 0 or self.canvas.coords(self.pipes[-1][0])[0] < self.width - 200:
         self.create_pipe()
-
       if self.check_collision():
         self.game_over = True
         self.show_game_over_message()
         print("Game Over! Your score was:", self.score)
-
       self.canvas.itemconfig(self.score_text, text=f"Счёт: {self.score}")
-
+      self.canvas.tag_raise(self.score_text)
       self.master.after(20, self.update)
 
   def check_collision(self):
@@ -111,23 +99,18 @@ class FlappyBird:
     bird_x = bird_coords[0]
     bird_y = bird_coords[1]
     bird_width = 50
-    bird_height = 50
-
+    bird_height = 45
     for top_pipe, bottom_pipe in self.pipes:
       top_coords = self.canvas.coords(top_pipe)
       bottom_coords = self.canvas.coords(bottom_pipe)
-
       if (bird_x + bird_width > top_coords[0] and bird_x < top_coords[0] + 50 and
           bird_y < top_coords[1] + 400):
         return True
-
       if (bird_x + bird_width > bottom_coords[0] and bird_x < bottom_coords[0] + 50 and
           bird_y + bird_height > bottom_coords[1]):
         return True
-
     if bird_y < 0 or bird_y + bird_height > self.height:
       return True
-
     return False
 
 if __name__ == "__main__":
